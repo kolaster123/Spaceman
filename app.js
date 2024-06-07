b970b29d07051bd9de5cc11b813059acb3883bec
kolaster123 <carbajalruben1563@gmail.com>
2024-06-07 12:15:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+create image for hangman
+remove limbs if letters not correct
+create clickable letters to the right of screen
+have a fill in the blank
+letter turn red if incorrect and gets crossed out
+letter turns green and shows up in the fill the blank
+player gets 5 tries before losing
+include win/lose sign after word filled out or not
+*/
