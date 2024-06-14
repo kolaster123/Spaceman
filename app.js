6d921001b7022a6b66dc6fb64907a587efb5b17e
kolaster123 <carbajalruben1563@gmail.com>
2024-06-13 21:24:41 -0700

    javascript WIP

--- app.js
+++ app.js
@@ -7,4 +7,45 @@ add letter containers to the right side and hangman on the left
 fill in the blank at the bottom 
 
 */
-const wordDisplay = document.querySelector(".word-display");
+const  wordDisplay= document.querySelector(".word-display");
+const  guesses= document.querySelector(".guesses-text b");
+const  keyboard= document.querySelector(".keyboard");
+const  hangmanImage= document.querySelector(".spaceman-container img");
+const  gamemodel= document.querySelector(".game.model");
+const  playAgain= gamemodel.querySelector(".button");
+
+let currentWord, correctLetters, WrongGuessCount;
+const maxGuesses = 6;
+
+const resetGame = () => {
+    correctLetters = [];
+    WrongGuessCount = 0
+    hangmanImage.src= "images/hangman-0.svg";
+    guesses.innerText = `${WrongGuessCount} / ${maxGuesses}`;
+
+
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li`).join("");
+
+    keyboard.querySelectorAll("button").forEach(btn => btn.disabled =false);
+
+    gamemodel.classList.remove("show");
+}
+
+resetGame();
+
+
+
+const worldList = [
+  {
+    word: "trex",
+    hint: "a big dinosaur"
+  },
+  {
+    word: "banana",
+    hint: "a fruit that is yellow"
+  },
+  {
+    word: "computer",
+    hint: "something that we are all using now"
+  },
+]
